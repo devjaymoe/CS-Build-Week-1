09eb1dfed46ae110a697958c18dd9d85c507640d
Devin_Moore <d.j.moore209@gmail.com>
2020-06-24 15:46:38 -0400

    added presets

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useCallback, useRef } from "react";
+import { heartPreset } from "./presets/hearts.js";
+import { pentaPreset } from "./presets/Penta-decathlon.js";
+import { gliderGun } from "./presets/gliderGun.js";
 import produce from "immer";
 
 const numRows = 25;
@@ -24,6 +27,27 @@ const generateEmptyGrid = () => {
 
     return rows;
 };
+// function to check all neighbors in the sim loop
+const countNeighbors = (grid, x, y) => {
+    return operations.reduce((acc, [i, j]) => {
+        const row = (x + i + numRows) % numRows;
+        const col = (y + j + numCols) % numCols;
+        acc += grid[row][col];
+        return acc;
+    }, 0);
+};
+
+const presetContainer = {
+    Hearts: heartPreset,
+    Penta: pentaPreset,
+    Glider: gliderGun,
+};
+
+const speeds = {
+    slow: 800,
+    normal: 200,
+    fast: 50,
+};
 
 function App() {
     const [grid, setGrid] = useState(() => {
@@ -34,15 +58,15 @@ function App() {
 
     const [gen, setGen] = useState(0);
 
+    const [speed, setSpeed] = useState("normal");
+
     // used in conjunction with run simulation
     // so that the state is up to date on the callback
     const runningRef = useRef(running);
     runningRef.current = running;
 
-    // generation tracker used with ref hook
-    // for the same reasons running ref is used
-    const genRef = useRef(gen);
-    genRef.current = gen;
+    const speedRef = useRef(speed);
+    speedRef.current = speed;
 
     const runSimulation = useCallback(() => {
         // kill command for recursive function
@@ -64,21 +88,7 @@ function App() {
                 // double for loop to go through every value in the grid
                 for (let i = 0; i < numRows; i++) {
                     for (let j = 0; j < numCols; j++) {
-                        let neighbors = 0;
-                        operations.forEach(([x, y]) => {
-                            const newI = i + x;
-                            const newJ = j + y;
-                            // checking the bounds
-                            if (
-                                newI >= 0 &&
-                                newI < numRows &&
-                                newJ >= 0 &&
-                                newJ < numCols
-                            ) {
-                                // if we have a live cell its going to add 1 to the neighbors
-                                neighbors += g[newI][newJ];
-                            }
-                        });
+                        let neighbors = countNeighbors(g, i, j);
 
                         if (neighbors < 2 || neighbors > 3) {
                             // if this case current position dies
@@ -92,52 +102,105 @@ function App() {
             });
         });
 
-        setTimeout(runSimulation, 500);
+        setTimeout(runSimulation, speeds[speedRef.current]);
     }, []);
+
+    const handleChange = (e) => {
+        let presetName = e.target.value;
+
+        if (presetName === "None") {
+            setGrid(generateEmptyGrid());
+        } else {
+            setGrid(presetContainer[presetName]);
+        }
+    };
+
+    const handleSpeed = (e) => {
+        setSpeed(e.target.value);
+    };
 
     return (
         <>
-            <button
-                onClick={() => {
-                    // changes running state
-                    setRunning(!running);
-                    if (!running) {
-                        runningRef.current = true;
-                        runSimulation();
-                    }
-                    console.log(grid);
-                }}
-            >
-                {/* checks running state and changes text */}
-                {running ? "Stop" : "Start"}
-            </button>
-            <button
-                onClick={() => {
-                    const rows = [];
-                    for (let i = 0; i < numRows; i++) {
-                        rows.push(
-                            Array.from(Array(numCols), () =>
-                                Math.random() > 0.8 ? 1 : 0
-                            )
-                        );
-                    }
+            <div className="controls">
+                <label htmlFor="slow">Slow</label>
+                <input
+                    type="radio"
+                    id="slow"
+                    name="speed"
+                    value="slow"
+                    onChange={handleSpeed}
+                />
+                <label htmlFor="normal">Normal</label>
+                <input
+                    type="radio"
+                    id="normal"
+                    name="speed"
+                    value="normal"
+                    defaultChecked
+                    onChange={handleSpeed}
+                />
+                <label htmlFor="fast">Fast</label>
+                <input
+                    type="radio"
+                    id="fast"
+                    name="speed"
+                    value="fast"
+                    onChange={handleSpeed}
+                />
+                <button
+                    onClick={() => {
+                        // changes running state
+                        setRunning(!running);
+                        if (!running) {
+                            runningRef.current = true;
+                            runSimulation();
+                        }
+                        console.log(grid);
+                    }}
+                >
+                    {/* checks running state and changes text */}
+                    {running ? "Stop" : "Start"}
+                </button>
+                <button
+                    onClick={() => {
+                        const rows = [];
+                        for (let i = 0; i < numRows; i++) {
+                            rows.push(
+                                Array.from(Array(numCols), () =>
+                                    Math.random() > 0.8 ? 1 : 0
+                                )
+                            );
+                        }
 
-                    setGrid(rows);
-                }}
-            >
-                Randomize
-            </button>
-            <button
-                onClick={() => {
-                    setGrid(generateEmptyGrid());
-                    setGen(0);
-                }}
-            >
-                Clear
-            </button>
-            <span>Current Generation: {gen}</span>
+                        setGrid(rows);
+                    }}
+                >
+                    Randomize
+                </button>
+                <button
+                    onClick={() => {
+                        setGrid(generateEmptyGrid());
+                        setGen(0);
+                        setRunning(false);
+                    }}
+                >
+                    Clear
+                </button>
+                <span>Current Generation: {gen}</span>
+                <label htmlFor="presets">Presets</label>
+                <select
+                    className="presets"
+                    id="presets"
+                    onChange={handleChange}
+                >
+                    <option value="None">...</option>
+                    <option value="Hearts">Hearts</option>
+                    <option value="Penta">Penta- decathlon</option>
+                    <option value="Glider">Gosper's Glider Gun</option>
+                </select>
+            </div>
             <div
-                className="App"
+                className="gameGrid"
                 style={{
                     // grid css
                     display: "grid",
